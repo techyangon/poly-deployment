a786d901b54e4bcc1c7221985f8168ffd4c3caa8
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-13 14:35:44 +0630

    Test VPC creation

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_vpc" "poly_asia" {
+  cidr_block = "172.16.0.0/16"
+}

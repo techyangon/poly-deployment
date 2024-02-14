62933bf50669292bb717bbd82c34810f53c07f2f
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-14 21:40:22 +0630

    Add EC2 instanace

--- main.tf
+++ main.tf
@@ -25,12 +25,32 @@ variable "WEB_ALLOCATION_ID" {
   type = string
 }
 
+variable "VPC_CIDR_BLOCK" {
+  type = string
+}
+
+variable "WEB_CIDR_BLOCK" {
+  type = string
+}
+
+variable "DB_CIDR_BLOCK" {
+  type = string
+}
+
+variable "RDS_MASTER_PASSWORD" {
+  type = string
+}
+
+variable "EC2_AMI_ID" {
+  type = string
+}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
 
 resource "aws_vpc" "poly_asia" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = var.VPC_CIDR_BLOCK
 }
 
 resource "aws_internet_gateway" "default" {
@@ -42,26 +62,17 @@ resource "aws_internet_gateway" "default" {
 
 resource "aws_subnet" "web" {
   vpc_id            = aws_vpc.poly_asia.id
-  cidr_block        = "172.16.0.0/21"
+  cidr_block        = var.WEB_CIDR_BLOCK
   availability_zone = "ap-southeast-1a"
   tags = {
     Name = "public"
   }
 }
 
-resource "aws_subnet" "db_1" {
+resource "aws_subnet" "db" {
   vpc_id            = aws_vpc.poly_asia.id
-  cidr_block        = "172.16.8.0/21"
+  cidr_block        = var.DB_CIDR_BLOCK
   availability_zone = "ap-southeast-1a"
-  tags = {
-    Name = "private"
-  }
-}
-
-resource "aws_subnet" "db_2" {
-  vpc_id            = aws_vpc.poly_asia.id
-  cidr_block        = "172.16.16.0/21"
-  availability_zone = "ap-southeast-1b"
   tags = {
     Name = "private"
   }
@@ -89,6 +100,16 @@ resource "aws_route_table" "db" {
     gateway_id     = aws_internet_gateway.default.id
     nat_gateway_id = aws_nat_gateway.public.id
   }
+}
+
+resource "aws_route_table_association" "default" {
+  subnet_id      = aws_subnet.web.id
+  route_table_id = aws_default_route_table.default.id
+}
+
+resource "aws_route_table_association" "db" {
+  subnet_id      = aws_subnet.db.id
+  route_table_id = aws_default_route_table.db.id
 }
 
 resource "aws_default_network_acl" "default" {
@@ -162,4 +183,17 @@ resource "aws_network_interface" "server" {
 resource "aws_eip_association" "server" {
   allocation_id        = var.WEB_ALLOCATION_ID
   network_interface_id = aws_network_interface.server.id
+}
+
+resource "aws_instance" "web" {
+  ami                                  = var.EC2_AMI_ID
+  instance_type                        = "t2.micro"
+  instance_initiated_shutdown_behavior = "terminate"
+  network_interface {
+    network_interface_id = aws_network_interface.server.id
+    device_index         = 0
+  }
+  tags = {
+    Name = "Web server"
+  }
 }

6f9c7bf51c7a94e547385182bc9d23aad88508a8
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-14 22:07:11 +0630

    Fix NAT gateway setup

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_route_table" "db" {
   vpc_id = aws_vpc.poly_asia.id
 
   route {
-    gateway_id     = aws_internet_gateway.default.id
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.public.id
   }
 }

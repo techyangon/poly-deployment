7f842d01a1186197ec45baf50a1c9dd7dcaf7760
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-02-14 10:01:27 +0630

    Add security groups for web server and DB instances

--- main.tf
+++ main.tf
@@ -46,3 +46,50 @@ resource "aws_default_network_acl" "default" {
     action     = "allow"
   }
 }
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.poly_asia.id
+
+  ingress {
+    self      = true
+    protocol  = -1
+    from_port = 0
+    to_port   = 0
+  }
+
+  ingress {
+    cidr_blocks = "0.0.0.0/0"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = "0.0.0.0/0"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+}
+
+resource "aws_security_group" "db" {
+  name        = "db_sg"
+  description = "Security group for DB instances"
+  vpc_id      = aws_vpc.poly_asia.id
+}
+
+resource "allow_vpc_security_group_ingress_rule" "allow_server" {
+  security_group_id = aws_security_group.db.id
+  ip_protocol       = "tcp"
+  from_port         = 5432
+  to_port           = 5432
+  security_group    = aws_security_group.default.id
+}
+
+resource "allow_vpc_security_group_egress_rule" "allow_server" {
+  security_group_id = aws_security_group.db.id
+  ip_protocol       = "tcp"
+  from_port         = 5432
+  to_port           = 5432
+  security_group    = aws_security_group.default.id
+}
